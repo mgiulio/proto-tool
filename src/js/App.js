e64e57f00badcd6c241818d614c6f03c0e667e6f
mgiulio <giulio.mainardi@gmail.com>
2015-11-16 14:28:07 +0100

    Remove old code

--- src/js/App.js
+++ src/js/App.js
@@ -25,62 +25,6 @@ var App = React.createClass({
 			translateDown: this.translateDown,
 			translateLeft: this.translateLeft,
 		};
-		
-		//this.setupKeyboardUI();
-	},
-	
-	setupKeyboardUI: function() {
-		document.addEventListener('keydown', this.onKeydown, false);
-	},
-	
-	onKeydown: function(e) {
-		//e.stopPropagation();
-		//e.preventDefault();
-		
-		console.log(e.keyIdentifier);
-		console.log(e);
-		
-		var
-			amount = 10,
-			multiplier = 10
-		;
-		
-		if (e.shiftKey)
-			amount *= multiplier;
-		else if (e.ctrlKey)
-			amount /= multiplier;
-		
-		switch (e.keyIdentifier) {
-			case 'Up':
-				this.translateUp(amount);
-				break;
-			case 'Right':
-				this.translateRight(amount);
-				break;
-			case 'Down':
-				this.translateDown(amount);
-				break;
-			case 'Left':
-				this.translateLeft(amount);
-				break;
-			case 'U+0009':
-				if (e.shiftKey)
-					this.selectPrev();
-				else
-					this.selectNext();
-				e.preventDefault();
-				break;
-			case 'U+0041': // a
-				//this.addObject('Rectangle', ...this.getMouseClientPos(), 100, 50);
-				
-				//let [x, y] = this.getMouseClientPos();
-				//this.addObject('Rectangle', x, y, 100, 50);
-				
-				//this.addObject('Rectangle', ...(this.getMouseClientPos().push(100, 50)));
-				
-				this.addObject('Rectangle', ...(this.getMouseClientPos().concat([100, 50])));
-				break;
-		}
 	},
 	
 	getMouseClientPos: function() {
@@ -112,13 +56,11 @@ var App = React.createClass({
 	selectPrev: function() {
 		this.select((this.state.selected - 1) % this.state.objects.length);
 	},
-	
 	setPos: function(x, y) {
 		this.state.objects[this.state.selected].setPos(x, y);
 		
 		this.forceUpdate();
 	},
-	
 	translate: function(x, y) {
 		this.state.objects[this.state.selected].translate(x, y);
 		
